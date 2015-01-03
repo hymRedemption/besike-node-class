50bcf651e6c9400cc29d2089b88f01c935f7698b
he.redemption <he.redemption@gmail.com>
2015-01-03 23:06:09 +0800

    class constructor

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = Class;
+
+
+function Class(classMember) {
+	var construct = null;
+	var varType;
+	// get the constructor
+	for ( var property in classMember ){
+		if ( property === "initialize" ){
+			construct = classMember[property];
+		}
+	}
+	if ( construct == null){//no constructor 
+		construct = function(){};
+	}
+	// add instance methods
+	for ( var method in classMember){
+		if ( method !== "initialize" ){
+			construct.prototype[method] = classMember[method];
+		}
+	}
+	return construct;
+};
