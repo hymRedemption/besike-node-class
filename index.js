9997929ee0a698eb646df9a2af3fca232ea1dc13
he.redemption <he.redemption@gmail.com>
2015-01-05 09:54:57 +0800

    Refactor the code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = Class;
 
-
 function Class(classMember, baseClass) {
 	var construct = null;
+
 	// get the constructor
 	if (classMember['initialize']){
 		construct = classMember['initialize'];
@@ -11,26 +11,49 @@ function Class(classMember, baseClass) {
 	else{
 		construct = function(){};
 	}
-	// add instance methods
-	for ( var method in classMember){
-		if ( method !== "initialize" ){
-			construct.prototype[method] = classMember[method];
-		}
-	}
+
+	// Implement the inheritance
 	if (baseClass){
-		function F(){
-			this.constructor = construct;
-		};
-		F.prototype = baseClass.prototype;
-		var temp = new F();
-		for( var proMethod in construct.prototype){
-			temp[proMethod] = construct.prototype[proMethod];
-		}
+		//create temp prototype to implement the inheritance
+		var temp = tempPrototype(construct, baseClass.prototype);
+		//add the instance method to the temp prototype
+		addInstanceMethod(temp, classMember);
 		construct.prototype = temp;
 		construct["__super__"] = baseClass;
 	}
 	else{
+		//add the instance method to the temp prototype
+		addInstanceMethod(construct.prototype, classMember);
 		construct["__super__"] = Object;
 	}
+	
+	//Implent the super
+	var current_class = construct;
+	construct.prototype.super = function(){
+		if (arguments.length != 0){
+			var funcName = arguments[0];
+			var result;
+			var old = current_class;
+			current_class = current_class.__super__;
+			result = current_class.prototype[funcName].apply(this, [].slice.call(arguments,1));
+			current_class = old;
+			return result;
+		}
+	};
 	return construct;
 };
+
+function addInstanceMethod(constrtPrototype, methods){
+	for( var method in methods){
+		constrtPrototype[method] = methods[method];
+	}
+}
+
+function tempPrototype(constructFunc, baseClassPrototype){
+	function F(){
+		this.constructor = constructFunc;
+	};
+	F.prototype = baseClassPrototype;
+	var o = new F();
+	return o;
+}
